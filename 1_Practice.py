c453776e26bf27337e890403dcebfb154333e411
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-17 19:27:37 +0000

    Update

---
+++ 1_Practice.py
@@ -0,0 +1,15 @@
+name = "Md. Shakil Khan"
+print(name.upper())
+print(name.lower())
+print(name.find('S'))
+print(name.replace("Khan","Ahmed"))
+
+print("Md" in name)
+
+print(11/2)
+print(11//2)
+print(11%2) #find reminder
+print(5**2) #power calculate
+name2=44
+print(name+str(name2))
+print(25<3 or 3>26)
