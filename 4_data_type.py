203267f629ff61974c8a6fdb60c154ecf81471c6
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-17 20:40:01 +0000

    Updated

---
+++ 4_data_type.py
@@ -0,0 +1,39 @@
+num1 = 10
+print(num1, "is of type",type(num1))
+num2 = 10.2
+print(num2, "is of type",type(num2))
+num3 = 10j
+print(num3, "is of type",type(num3))
+print("\n")
+#List Data Type
+x= "This is python"
+print(x)
+print(type(x))
+print("\n")
+Languages = ["Java", "Python", "C"]
+print(Languages[0])
+print(Languages[2])
+print(Languages)
+print(type(Languages))
+
+print("\n")
+Languages = ("Java", "Python", "C")
+print(Languages)
+print(type(Languages))
+
+print("\n")
+x = range(6)
+print(x)
+print(type(x))
+
+print("\n")
+x = {"Name ":"Rahim", "Age ":40}
+print(x)
+print(type(x))
+
+print("\n")
+x = {"Java", "Python", "C"}
+print(x)
+print(type(x))
+
+
