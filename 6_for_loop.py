6ea31f4d9b3a9299b85433ceffa987d82da902f8
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-18 17:22:49 +0000

    Updated

---
+++ 6_for_loop.py
@@ -0,0 +1,4 @@
+languages = ['C', 'C++','Java','Python']
+
+for language in languages:
+    print(language)
---
+++ 7_area_of_shape.py
@@ -0,0 +1,28 @@
+print("\nArea of Trinagle: ")
+base = float(input("Enter Base: "))
+height= float(input("Enter Height: "))
+
+area = 0.5* base*height
+print("Area: ",area)
+
+print("\nArea of Rectangle: ")
+width = int(input("Enter the width: "))
+height = int(input("Enter the height: "))
+area= width * height
+print("Area: ",area)
+
+print("\nArea of Square: ")
+length= int(input("Enter Length: "))
+area = length * length
+print("Area: ",)
+
+print("\nArea of Parallelogram: ")
+base= int(input("Enter base: "))
+height= int(input("Enter vertial height: "))
+area= base*height
+print("Area: ",area)
+
+print("\nArea of Circle: ")
+radius= float(input("Enter the radius: "))
+area= 3.1416 * radius *radius
+print("Area: ",area)
