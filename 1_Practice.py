8e21a814fae64b4ce4cd01dae1862c81315aa5b3
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-18 16:31:25 +0000

    Updated

--- 1_Practice.py
+++ 1_Practice.py
@@ -13,3 +13,10 @@ print(5**2) #power calculate
 name2=44
 print(name+str(name2))
 print(25<3 or 3>26)
+
+#Print multiple print() function in single line using end parameter
+print('Good Morning!', end=" ")
+print('It is rainy today')
+
+#Used for seperate line using sep parameter
+print("This is Python","Python is awesome!", sep=". ")
---
+++ 5_operators.py
@@ -0,0 +1,41 @@
+#Arithmetic Operators
+a= 7
+b=5
+print("Arithmetic Operators.")
+print("Addition: ", a+b)
+print("Subtraction: ", a-b)
+print("Multiplication: ", a*b)
+print("Division: ",a/b)
+print("Floor Division: ",a//b)
+print("Modulo: ",a%b)
+print("Power- a to b: ",a**b)
+print("\n")
+
+#Assignment Operators
+print("Assignment Operators.")
+a= 5
+b=2
+a=b
+print("Assignment Operator: ", a)
+a= 9
+a+=b
+print("Addition Assignment: ",a)
+a= 12
+a-=b
+print("Subtraction Assignment: ",a)
+a= 23
+a*=b
+print("Multiplication Assignment: ",a)
+a= 34
+a/=b
+print("Division Assignment: ",a)
+a= 33
+a%=b
+print("Remainder Assignment: ",a)
+a= 7
+a**=b
+print("Exponent Assignment: ",a)
+print("\n")
+
+#Comparison Operators
+print("Assignment Operators.")
