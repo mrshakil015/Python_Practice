343775cfc4ef6c31245a78c97223011116d6330f
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-19 04:18:37 +0000

    Updated

---
+++ 9_function.py
@@ -0,0 +1,25 @@
+def my_function1():
+    print("Hello, This function.")
+my_function1()
+
+def my_function2(fname, lname):
+    print(fname + " "+ lname)
+my_function2("Md.","Shakil")
+
+#Arbitrary Arguments
+def my_function3(*name):
+    print("The adult child is: ", name[2])
+my_function3("Rahim", "Karim","Abdul")
+
+#Keyword Arguments
+def my_function4(name1, name2,name3):
+    print("The adult child is: "+ name1)
+my_function4(name1= "Rahim", name2= "Karim", name3= "Abdul")
+
+def print_sum(num1, num2):
+    print("Result: ",num1 + num2)
+print_sum(5,8)
+
+def print_sum2(num1, num2=20):
+    print("Result: ",num1 + num2)
+print_sum2(5)
