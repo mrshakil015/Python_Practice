197a0aa57326fc8be47743f128a96cfdf145acfe
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-19 21:39:52 +0000

    Updated

--- 12_pandas.py
+++ 12_pandas.py
@@ -5,6 +5,7 @@ student_dict = {
     "Name":['Shakil','Ridoy','Shaiful','Banna'],
     "ID":[15,11,14,13],
     "Address":['Hemayetpur','Kollanpur','Savar','Mirpur'],
+    "Age":[23,24,26,25]
 }
 students_tab = pd.DataFrame(student_dict)
 print("Students List:\n",students_tab)
@@ -12,3 +13,8 @@ students_tab.to_csv('list.csv')
 students_tab.to_csv('list_without_index.csv', index=False)
 print("Show the first 2 rows:\n",students_tab.head(2))
 print("Show the last 2 rows:\n",students_tab.tail(2))
+print("\nDescribe the data frame:\n",students_tab.describe())
+studentList2 = pd.read_csv('StudentList.csv')
+print("\nStudents List 2:\n",studentList2)
+print("Print the student list 2 Marks:\n",studentList2['Roll NO'])
+print("Print 1 index student roll:",studentList2['Roll NO'][1])
