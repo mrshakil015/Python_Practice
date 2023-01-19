37f12afdd9a3039626a08acff133f9e08225fcf8
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-19 03:36:04 +0000

    Updated

--- 8_math_library.py
+++ 8_math_library.py
@@ -3,3 +3,18 @@ print("The value of PI: ",math.pi)
 print("Maximum value: ",max(50,60))
 print("Minimum value: ",min(50,60))
 
+#Numeric Function
+print("Floor the value: ",math.floor(4.5))
+print("Ceil the value: ",math.ceil(4.5))
+print("Factorial the number: ",math.factorial(4))
+print("GCD of 4 and 20 is: ",math.gcd(20,4))
+print("The absolute value of -8 is: ", math.fabs(-8.4))
+print("The remainder of 21/4: ", math.fmod(21,4)) #Return the remainder
+print("Sum of all items: ",math.fsum([2,5,7,8])) #for list
+num = (10,15,30)
+print("Sum of all items: ",math.fsum(num)) #for tuples
+print("The natural logarithm: ", math.log(7))
+print("Power value: ",math.pow(5,2)) #Returns the value of x to the power of y
+print("Convert degree into radians: ",math.radians(70))
+print("Remainder: ",math.remainder(21,4))
+
