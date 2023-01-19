811b0821d97d6f83436e25b5b8c3382f8911a293
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-19 04:24:27 +0000

    Updated

--- 9_function.py
+++ 9_function.py
@@ -12,8 +12,10 @@ def my_function3(*name):
 my_function3("Rahim", "Karim","Abdul")
 
 #Keyword Arguments
-def my_function4(name1, name2,name3):
-    print("The adult child is: "+ name1)
+def my_function4(name1, name2, name3):
+    print("The first child is: "+ name1)
+    print("The second child is: "+ name2)
+    print("The third child is: "+ name3)
 my_function4(name1= "Rahim", name2= "Karim", name3= "Abdul")
 
 def print_sum(num1, num2):
