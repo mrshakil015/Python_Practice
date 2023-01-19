3c0a769d0e575ccec9387a665c678f2fb29939d4
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-19 17:10:21 +0000

    Updated

--- 11_numpy.py
+++ 11_numpy.py
@@ -2,5 +2,17 @@ import numpy as np
 
 array1 = np.array([1,2,3,4])
 print("One dimensional array",array1)
-print(array1.dtype)
+
+listarray = np.array([(1,2,3,8),(5,6,4,7),(3,5,7,2)])
+print("Original array: ",listarray)
+print("Array dimension: ",listarray.ndim)
+print("Array Data Type: ",listarray.dtype)
+print("Array item size: ",listarray.itemsize)
+print("Array size: ",listarray.size)
+print("Array shape: ",listarray.shape)
+listarray2 = np.array([(1,2,3,8),(5,6,4,7),(3,5,7,2)])
+print("Original array:\n",listarray2)
+print("Reshape the array:\n",listarray2.reshape(6,2))
+array3= listarray2.copy()
+print("Copy value one array to another:\n",array3)
 
