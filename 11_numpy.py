32741d193237e83d9f7aeb40e6c2599f3c0739bf
Md. Shakil <98937877+mrshakil015@users.noreply.github.com>
2023-01-19 19:22:35 +0000

    Updated

--- 11_numpy.py
+++ 11_numpy.py
@@ -10,13 +10,13 @@ print("Array Data Type: ",listarray.dtype)
 print("Array item size: ",listarray.itemsize)
 print("Array size: ",listarray.size)
 print("Array shape: ",listarray.shape)
-listarray2 = np.array([(1,2,3,8),(5,6,4,7),(3,5,7,2)])
+listarray2 = np.array([(1,2,3,8),(5,6,54,7),(3,5,7,2)])
 print("Original array:\n",listarray2)
 print("Reshape the array:\n",listarray2.reshape(6,2))
 array3= listarray2.copy()
 print("Copy value one array to another:\n",array3)
-array4 = np.array([5,8,97,7])
-print("Another array: ",array4)
+array4 = np.array([25,8,97,16])
+print("Array4: ",array4)
 array4[1] = 44
 print("Another array: ",array4)
 
@@ -27,4 +27,14 @@ print("Iterate on the elements of the 2-D array")
 for x in array3:
     for y in x:
         print(y)
-        
+
+
+print("Array3: ",array3)
+print("Find max number from Array3: ",array3.max())
+print("Find min number from Array3: ",array3.min())
+print("Summation of Array3: ",array3.sum())
+print("Column wise Summation of Array3: ",array3.sum(axis=0))
+print("Row wise Summation of Array3: ",array3.sum(axis=1))
+print("Square root of array4: ",np.sqrt(array4))
+print(np.std(array3))
+
